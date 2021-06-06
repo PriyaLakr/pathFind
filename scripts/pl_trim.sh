117b1a46d155e69528c3a5a19cc81218149205da
PriyaLakra <66781257+PriyaLakr@users.noreply.github.com>
2021-06-06 19:10:52 +0530

    Update pl_trim.sh

--- scripts/pl_trim.sh
+++ scripts/pl_trim.sh
@@ -1,13 +1,41 @@
 #!/bin/bash
 
-# usage: bash pl_trim.sh [infile path] [threads]
+help(){
+  echo ""
+  echo -e "i (input_dir)      path to the directory containing input files"
+  echo -e "s (infile_suffix)  suffix of input files; eg, for paired-end reads, some_name_R1.fastq.gz some_name_R2.fastq.gz, suffix would be R2.fastq.gz"
+  echo -e "t (num_thread)     number of threads"
+  echo -e "k (keepBothReads)  Type TRUE to keep both reads after trimming"
+  echo -e "a (adapter_file)   name of the adapter file"
+  echo -e "m (min_len)        min read length feature"
+  echo -e "\tensure that raw files and adaptor file for trimmomatic are in the same input directory"
+  exit 1
+  
+}
 
-cd $1
-mkdir trim_out
-trim=$1/trim_out
+while getopts "i:s:t:o:m:a:l:" opt; do
+    case "$opt" in:
+      i ) input_dir="$OPTARG" ;;
+      s ) infile_suffix="$OPTARG" ;;
+      t ) num_thread="$OPTARG" ;;
+     # o ) output_dir="$OPTARG" ;;
+      k ) keepBothReads="$OPTARG" ;;
+      a ) adapter_file="$OPTARG" ;;
+      l ) min_len="$OPTARG" ;;
+      \? ) help; exit 1 ;;
+     esac
+   done
+   
+cd $input_dir
+mkdir -p $input_dir/trim_out
 
-for infile in *_R1_final.fastq.gz;
-do  name=$(basename $infile _R1_final.fastq.gz); trimmomatic PE -threads $2 ${name}_R1_final.fastq.gz ${name}_R2_final.fastq.gz  $trim/${name}_R1_trim.fastq.gz  $trim/${name}_R1_untrim.fastq.gz  $trim/${name}_R2_trim.fastq.gz  $trim/${name}_R2_untrim.fastq.gz ILLUMINACLIP:TruSeq3-SE.fa:2:30:10 MINLEN:30;
+start=$(date +%s)
+
+for infile in *${infile_suffix};
+  do name=$(basename $infile ${infile_suffix}); trimmomatic PE -threads $num_thread ${name}${infile_suffix} ${name}${infile_suffix}  $input_dir/trim_out/${name}_R1_trim.fastq.gz  $input_dir/trim_out/${name}_R1_untrim.fastq.gz  $input_dir/trim_out/${name}_R2_trim.fastq.gz  $input_dir/trim_out/${name}_R2_untrim.fastq.gz ILLUMINACLIP:${adapter_file}:2:30:10:8:${keepBothReads} MINLEN:${min_len}
 done
 
-echo "Done!"
+end=$(date +%s)
+
+echo -e "\tProcess completed in ( ($end - $start)/60 ) minutes"
+
